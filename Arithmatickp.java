e856add27c45ab72249122dfc5ef848833d2ba98
Prakashkumar2001 <prakashvalarmathi2001@gmail.com>
2023-12-29 13:44:39 +0530

    Arithmati coperation

---
+++ Arithmatickp.java
@@ -0,0 +1,11 @@
+ public class Arithmatickp {
+    public static void main(String[] args) {
+        int a=9;
+        int b=89;
+        int c=a+b;
+        int d=c*b;
+        int k=a/b;
+        int m=b%a;
+        System.out.println(a+" "+b+" "+c+" "+d+" "+k+" "+m);
+    }
+}
